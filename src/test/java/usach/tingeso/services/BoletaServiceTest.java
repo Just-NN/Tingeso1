8dbc692bf54181611074b6817c850664fd59dd73
Ivan Guajardo Arias <ivan.guajardo.a@usach.cl>
2024-04-24 00:22:10 -0400

    [IG] Avancé mucho, reformulé mucho, me estanqué más o menos donde mismo
    Sigo sin poder hacer los tests para los repositorios
    Controllers: 90 93 95
    Services: 9 23 91 100
    Mañana tengo que terminar definitivamente mis tests y empezar el front, el front debiera sacarlo el jueves máximo y empezar con docker

--- src/test/java/usach/tingeso/services/BoletaServiceTest.java
+++ src/test/java/usach/tingeso/services/BoletaServiceTest.java
@@ -1,21 +1,2 @@
-package usach.tingeso.services;
-import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.Assertions.assertThat;
-
-
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import usach.tingeso.repositories.BoletaRepository;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
-public class BoletaServiceTest {
-
-    @Autowired
-    BoletaService boletaService;
-
-    @MockBean
-    BoletaRepository boletaRepository;
-
+package usach.tingeso.services;public class BoletaServiceTest {
 }
